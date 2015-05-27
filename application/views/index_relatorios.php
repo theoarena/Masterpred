6e1913822916d232ff0fac296c4aa9ea707a7dc4
Théo <theoarena@gmail.com>
2015-05-27 19:32:32 -0300

    Correçao INDEX

--- application/views/index_relatorios.php
+++ application/views/index_relatorios.php
@@ -2,7 +2,7 @@
 <html xmlns='http://www.w3.org/1999/xhtml'>
 <head>
 <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1' />
-	<title><?php Kohana::$config->load('config')->get('site_name'); ?></title>
+	<title><?php echo Kohana::$config->load('config')->get('site_name'); ?></title>
 
 	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
 	<meta charset='utf-8'>
